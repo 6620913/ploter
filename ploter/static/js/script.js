76e879d735c88d49c8bf5f8e2a98a5fc685487d0
NEERAJ <6620913@gmail.com>
2022-06-06 17:17:19 +0530

    found a way to make it as single page application

---
+++ ploter/static/js/script.js
@@ -0,0 +1,12 @@
+
+
+
+function poly(){
+document.getElementById("home").style.display='none';
+
+const c = document.getElementById("poly");
+c.style.display="block";
+console.log('executing poly');
+
+
+}
